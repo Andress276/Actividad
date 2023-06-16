9a3a697ccede2b8b558a72b2d6ad824370ae15e6
Andres276276 <113720551+Andres276276@users.noreply.github.com>
2023-06-16 00:19:47 -0500

    first commit

---
+++ app/Http/Controllers/Cursocontroler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Cursocontroler extends Controller
+{
+
+    public function numerosAmigos()
+    {
+        $numero1 = 220;
+        $numero2 = 284;
+
+        if ($this->sonAmigos($numero1, $numero2)) {
+            $mensaje = "$numero1 y $numero2 son números amigos.";
+        } else {
+            $mensaje = "$numero1 y $numero2 no son números amigos.";
+        }
+
+        return $mensaje;
+    }
+
+    private function sonAmigos($numero1, $numero2)
+    {
+        $sumaDivisores1 = array_sum($this->encontrarDivisores($numero1));
+        $sumaDivisores2 = array_sum($this->encontrarDivisores($numero2));
+
+        return ($sumaDivisores1 == $numero2) && ($sumaDivisores2 == $numero1);
+    }
+
+    private function encontrarDivisores($numero)
+    {
+        $divisores = [];
+
+        for ($i = 1; $i <= $numero / 2; $i++) {
+            if ($numero % $i == 0) {
+                $divisores[] = $i;
+            }
+        }
+
+        return $divisores;
+    }
+
+
+}
+
